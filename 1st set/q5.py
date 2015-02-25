dd0acec2e293cddc672effab2756f3a18a7eb01f
zyanki <yuki_muramatsu@cyberagent.co.jp>
2015-02-25 15:00:38 +0900

    no message

--- 1st set/q5.py
+++ 1st set/q5.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 # coding: utf-8
-
+# python q5.py < /Users/a18818/NLP100knock/data\ set/KEN_ALL.txt 1
 import sys
 buffer = sys.stdin.read().splitlines()
 head_number = sys.argv[1]
